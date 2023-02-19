04b892f75bcf322115903e65023733a38b26b1bc
NaashNix <thilakshanaravindu@gmail.com>
2023-02-19 11:11:04 +0530

    UI Done

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -3,7 +3,7 @@ import style from './InputBox.module.css';
 
 const InputBox = () => {
    return ( 
-      <div>
+      <div className={style.outer_div}>
          <input className={style.input_style} type="text" placeholder='Input Text' />
          <button className={style.button_style} type="button">ADD</button>
       </div>
